3b624de5a6154baa3cd559956cc1bf2c3e435903
Omkar Nitin Pathak <omiipathak2000@gmail.com>
2023-09-15 21:22:45 +0530

    added new print statement

--- src/main/java/mypkg/MyClass.java
+++ src/main/java/mypkg/MyClass.java
@@ -17,6 +17,7 @@ public class MyClass {
 	public void show()
 	{
 		System.out.println("Hello GFG");
+		System.out.println("Welcome to Jenkins Pipeline");
 	}
 	public static void main(String[] args) {
 		MyClass ob = new MyClass();
