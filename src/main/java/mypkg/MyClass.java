ecb8640dfe90a1d43c9da39e302b2aa19c7bf7c6
Omkar Nitin Pathak <omiipathak2000@gmail.com>
2023-09-15 18:57:39 +0530

    triangle area added

--- src/main/java/mypkg/MyClass.java
+++ src/main/java/mypkg/MyClass.java
@@ -10,7 +10,10 @@ public class MyClass {
 	{
 		return (s * s);
 	}
-	
+	public float areaTriangle(float r)
+	{
+		return (float) (3.14 * r * r);
+	}
 	public static void main(String[] args) {
 		MyClass ob = new MyClass();
 		int l;
