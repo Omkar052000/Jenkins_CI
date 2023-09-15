0827c12d7b0bbe53c839df053db16262d74fec16
Omkar Nitin Pathak <omiipathak2000@gmail.com>
2023-09-15 17:51:12 +0530

    first commit

---
+++ src/main/java/mypkg/MyClass.java
@@ -0,0 +1,27 @@
+package mypkg;
+import java.util.Scanner;
+
+public class MyClass {
+	public int areaRect(int l , int b)
+	{
+		return (l * b);
+	}
+	public int areaSquare(int s)
+	{
+		return (s * s);
+	}
+	
+	public static void main(String[] args) {
+		MyClass ob = new MyClass();
+		int l;
+		int b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Length :"); 
+		l = sc.nextInt();
+		System.out.println("Enter Breadth :"); 
+		b = sc.nextInt();
+		System.out.println("Area of the Rectangle is :"+ob.areaRect(l, b));
+		System.out.println("Area of the Square is :"+ob.areaSquare(10));
+	}
+
+}
